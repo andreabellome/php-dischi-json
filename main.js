e04d7218b00f96e1f15cbdf12839d96e63bf5663
Andrea Bellome <andrea.bellome@gmail.com>
2023-05-24 16:47:05 +0200

    debug click functions

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ createApp({
             classDispNone: 'disp-none',
             isActive: true,
             dataSingleCard: '',
-            idxactive: 0
+            idxactive: 0,
+            haveChecked: 0
         }
     },
     methods: {
@@ -22,6 +23,8 @@ createApp({
         /* start: at click on cross, the disc info disappears */
         closeWindowOnClick() {
             this.isActive = !this.isActive;
+            this.idxactive = 0;
+            this.haveChecked = 0;
         },
         /* end: at click on cross, the disc info disappears */
 
@@ -29,12 +32,18 @@ createApp({
         closeWindowOnClickMain() {
 
             if (this.dataSingleCard != '') {
-
+                console.log(this.idxactive)
                 this.idxactive = this.idxactive + 1;
 
-                if (this.idxactive == 2) {
+                if (this.idxactive == 2 && this.haveChecked == 1) {
                     this.isActive = !this.isActive;
+                    console.log(this.idxactive)
+                    this.idxactive = 0;
+                    this.haveChecked = 0;
                 }
+
+
+
             }
 
         },
@@ -47,6 +56,7 @@ createApp({
                 this.dataSingleCard = this.data[index];
             }
             this.idxactive = 0;
+            this.haveChecked = 1;
         }
         /* end: at click on disc car, the disc info appears */
 
